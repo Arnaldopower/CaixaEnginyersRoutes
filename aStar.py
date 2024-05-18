f72e72d8564aad3b1dfb972f329fc8d4c16f1cc5
Arnaldopower <116560232+Arnaldopower@users.noreply.github.com>
2024-05-19 01:30:02 +0200

    tinc son

--- aStar.py
+++ aStar.py
@@ -10,19 +10,24 @@ def main():
         dataDict[int(lot)][int(bloc)].append(node(municipi,cp.zfill(5),comarca,temps))
     
 
-    for i in range(0,3):
-        for j in range(0,4):
-            
+
+    for i in [2,4,5]:
+        for j in [1,2,3,4]:
+            open = [] #aqui li fotre l'inicial, a la funció el treiem però no el fiquem a close
+            heapq.heapify(open)
+            close = []
+            graph = dataDict[i][j]
             for z in range(0,5):
+                cost = 0
+                open = [] #aqui li fotre l'inicial, a la funció el treiem però no el fiquem a close
+                heapq.heapify(open)
+                StarAlgorithm( ,graph)
 
-
-
-
-def StarAlgorithm(node_inicial: node, directori):
-    open = []
-    close = []
-    heapq.heapify(open)
-
+def StarAlgorithm(node_inicial: node, graph: List[node]):
+    while open:
+        inici = heapq.heappop(open)
+        for fill in inici.connections:
+            
 
 
 
