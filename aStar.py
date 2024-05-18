2c52cd0f84254d5b4b41f1710d847c418f034600
elver <joelros2003@gmail.com>
2024-05-18 15:01:52 +0200

    feat: data loading

--- aStar.py
+++ aStar.py
@@ -1,6 +1,8 @@
 from elements import node, getDirections
+from loader import read_tsv
 
 def main():
+    read_tsv("input.tsv")
     origin = node("BellPrat", "Anoia", 1, 30)
     destiny = node("El Bruc", "Anoia", 1, 30)
     getDirections(origin, destiny)
