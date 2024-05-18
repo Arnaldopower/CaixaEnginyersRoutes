0422e5429764e1533c22f38d5ad206f2fe91a7ba
elver <joelros2003@gmail.com>
2024-05-18 18:12:50 +0200

    feat: make graph

--- aStar.py
+++ aStar.py
@@ -1,11 +1,25 @@
 from elements import node, getDirections
 from loader import read_tsv
+from cp_conv import nie_to_cp
 
 def main():
-    read_tsv("input.tsv")
-    origin = node("BellPrat", "Anoia", 1, 30)
-    destiny = node("El Bruc", "Anoia", 1, 30)
-    getDirections(origin, destiny)
+    data = read_tsv("input.tsv")
+    dataDict = {j:{i:[] for i in [1,2,3,4]} for j in [2,4,5]}
+    for lot,bloc,comarca,cp,municipi,_,temps in data:
+        dataDict[int(lot)][int(bloc)].append(node(f"{nie_to_cp(cp.zfill(5))}, {municipi}",comarca,temps))
+    with open("vertexs.txt","a", encoding="utf-8") as f:
+        for lot in dataDict.values():
+            for bloc in lot.values():
+                visited = set()
+                for origin in bloc:
+                    visited.add(origin)
+                    for destiny in bloc:
+                        if destiny in visited:
+                            continue
+                        else:
+                            lol = getDirections(origin, destiny)
+                            print(f"(({origin},{destiny}),{lol})")
+                            f.write(f"(({origin},{destiny}),{lol})\n")
 
 if __name__ == '__main__': 
     main()
--- elements.py
+++ elements.py
@@ -2,24 +2,25 @@ import googlemaps
 from datetime import datetime
 
 class node():
-    def __init__(self, municipi: str, comarca: str, bloc: int, estanciaMin: int):
-        self.municipi = municipi
+    def __init__(self, municipiCP: str, comarca: str, estanciaMin: int):
+        self.municipiCP = municipiCP
         self.comarca = comarca
-        self.bloc = bloc
         self.estanciaMin = estanciaMin
 
-    def get_mun(self) -> int:
-        return self.municipi 
-    def get_com(self) -> int:
+    def get_munCP(self):
+        return self.municipiCP
+    def get_com(self):
         return self.comarca
-    def get_bloc(self) -> int:
+    def get_bloc(self):
         return self.bloc
-    def get_estanciaMin(self) -> int:
+    def get_estanciaMin(self):
         return self.estanciaMin
+    def __str__(self):
+        return self.municipiCP
 
 def getDirections(origin: node, destiny: node) -> tuple[int, int]:
     now = datetime.now()
     gmaps = googlemaps.Client(key='AIzaSyAoFEZk9cyUV2LPi-k89aQp8f8TGdl42n0')
-    directions_result = gmaps.directions(origin.get_mun(),destiny.get_mun(),mode="driving",departure_time=now)
+    directions_result = gmaps.directions(origin.get_munCP(),destiny.get_munCP(),mode="driving",departure_time=now)
     return (directions_result[0]["legs"][0]["duration_in_traffic"]["value"], directions_result[0]["legs"][0]["distance"]["value"]) #value -> seconds / distance -> meters
 
